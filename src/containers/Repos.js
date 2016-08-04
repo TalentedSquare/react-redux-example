159d76551dfd34843fd1dc8b6b283085687690e9
furkanedu <furkan.orhan@boun.edu.tr>
2016-08-05 02:44:18 +0300

    repos are being showed in table with name and url, ItemsTable component added

--- src/containers/Repos.js
+++ src/containers/Repos.js
@@ -5,14 +5,20 @@ import { Link } from 'react-router'
 
 import app from './../app';
 
+
 @connect((state) => ({
     repos : state.reposReducer
 }))
+
 export default class Repos extends Component {
-
+	constructor(props) {
+		super(props);
+		this.actions = bindActionCreators(app.actions, this.props.dispatch);
+	}
+  componentDidMount() {
+  	this.actions.fetchRepos();
+  }
   render() {
-
-    const actions = bindActionCreators(app.actions, this.props.dispatch);
     const repos = this.props.repos;
     return (
       <div>
@@ -20,16 +26,43 @@ export default class Repos extends Component {
           actions.fetchRepos()
         }}>CLICK TO FETCH </div>
         <div>
-          { repos.loading ? "loading repos" : (repos.loaded ? "loaded " +repos.repos.total_count : "")}
+          { repos.loading ? "loading repos" : (repos.loaded ? repos.repos.items.length + " repositories loaded" : "")}
         </div>
         {	
-        	repos.loaded && repos.repos.items.map((item,i)=>{
-        		return <p key={i}>{ item.full_name }</p>
-        	}) 
+        	repos.loaded &&  <ItemsTable items={repos.repos.items}/>
         }
+        
         <br/><br/>
       </div>
     );
 
   }
+}
+class ItemsTable extends Component {
+	render(){
+		return (
+			<div>
+				<table>
+					<tbody>
+						<tr>
+							<th>index</th>
+							<th>repository name</th>
+							<th>link</th>
+						</tr>
+						{
+							this.props.items.map((item,i)=>{
+				        		return <tr key={i}>
+				        				<td>{ i + 1}</td>
+				        				<td>{ item.full_name }</td>
+				        				<td>
+				        					<a href={item.html_url}><b>link to repo</b></a>
+				        				</td>
+				        			</tr>
+				        	}) 
+				        }
+				    </tbody>
+				</table> 
+			</div>
+		)
+	}
 }
